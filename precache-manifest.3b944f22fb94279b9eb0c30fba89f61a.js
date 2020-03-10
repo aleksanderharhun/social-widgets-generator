b63f35a4e528436002ab20e10074db267252efe1
Aleksander Harhun <aleksanderharhun@gmail.com>
2020-03-10 21:53:37 +0100

    Updates

---
+++ precache-manifest.3b944f22fb94279b9eb0c30fba89f61a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e26c743b1479b94bd0e8c65ea10feb13",
+    "url": "/social-widgets-generator/index.html"
+  },
+  {
+    "revision": "f16cc76a8c1b4a6761ae",
+    "url": "/social-widgets-generator/static/css/main.ac260bae.chunk.css"
+  },
+  {
+    "revision": "900b2704389ab0d56350",
+    "url": "/social-widgets-generator/static/js/2.bf704df8.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/social-widgets-generator/static/js/2.bf704df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f16cc76a8c1b4a6761ae",
+    "url": "/social-widgets-generator/static/js/main.c7664264.chunk.js"
+  },
+  {
+    "revision": "0b441bf7f2a80a5e7c43",
+    "url": "/social-widgets-generator/static/js/runtime-main.67437f24.js"
+  }
+]);
